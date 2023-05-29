7e03312417d59349a305db510e823bc77854880b
Septian Dwi Cahyo <112248603+septiandwica@users.noreply.github.com>
2023-05-29 15:11:42 +0700

    update

--- src/js/main.js
+++ src/js/main.js
@@ -4,7 +4,7 @@
     hour = minute * 60,
     day = hour * 24;
 
-  let birthday = "Aug 28 00:00:00",
+  let birthday = "Aug 28, 2023 00:00:00",
     countDown = new Date(birthday).getTime(),
     x = setInterval(function () {
       let now = new Date().getTime(),
